c7f9ac416767a2cfff360b2dbe479938c3bfe665
maurojcordoba <mauro.j.cordoba@gmail.com>
2023-09-10 22:00:11 -0300

    cambios en cron

--- api/cron.js
+++ api/cron.js
@@ -1,42 +1,58 @@
 const { Telegraf } = require('telegraf');
+const cheerio = require('cheerio');
 const fs = require('fs');
 const chats = require('./chats');
-const planeton = require('./planeton');
 const path = require('path');
 
 require('dotenv').config();
 
-export default function handler(req, res) {
-  console.log("Inicio cron");
-  const dataPath = path.join(__dirname, '/data/data.json');
-  const bot = new Telegraf(process.env.BOT_TOKEN);
-  const data_chats = chats.getAll();
+const dataPath = path.join(__dirname, '/data/data.json');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const data_chats = chats.getAll();
+const data = JSON.parse(fs.readFileSync(dataPath));
 
-  planeton.getNews().then((news) => {
-    let data = JSON.parse(fs.readFileSync(dataPath));
-console.log(data);
-    //novedades
-    news.forEach((item) => {
-      const found = data.find((game) => game.id == item.id);
+export default async function handler(req, res) {
+  try {
+    fetch('https://www.planetongames.com/es/')
+      .then((response) => response.text())
+      .then((html) => {
+        let metadata = [];
+        let $ = cheerio.load(html);
 
-      if (!found) {
-        let captionText = `[${item.title}](${item.link})\nPrecio: ${item.price} - _Sin login_`;
-        let photoId = item.img;
+        $('article').each((i, element) => {          
+          let id = $(element).attr('data-id-product');
+          let img = $(element).find('img').attr('src');
+          let title = $(element).find('h3.elementor-title').text();
+          let price = $(element).find('span.elementor-price').text();
+          let link = $(element).find('a.elementor-product-link').attr('href');
 
-        data_chats.forEach((chat_id) =>
-          bot.telegram
-            .sendPhoto(chat_id, photoId, { caption: captionText, parse_mode: 'Markdown' })
-            .then(() => console.log('Mensaje enviado: ', item.title))
-            .catch((error) => console.error('Error al enviar el mensaje:', item.title, error))
-        );
-      }
-    });
-    fs.writeFileSync(dataPath, JSON.stringify(news));
-  });
+          metadata.push({ id, img, title, link, price });
+        });
 
-  process.once('SIGINT', () => bot.stop('SIGINT'));
-  process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
-  res.status(200).end('Hello Cron!');
+        //busco y envio novedades
+        metadata.forEach((item) => {
+          const found = data.find((game) => game.id == item.id);
+          
+          if (!found) {
+            console.log('DEBO Enviar:', item.title);
+            let captionText = `[${item.title}](${item.link})\nPrecio: ${item.price} - _Sin login_`;
+            let photoId = item.img;
+            
+            data_chats.forEach((chat_id) =>{
+              console.log(chat_id,chat_id);
+              bot.telegram
+                .sendPhoto(chat_id, photoId, { caption: captionText, parse_mode: 'Markdown' })
+                .then(() => console.log('Mensaje enviado: ', item.title))
+                .catch((error) => console.error('Error al enviar el mensaje:', item.title, error))
+            }
+            );
+          }
+        });
+        //fs.writeFileSync(dataPath, JSON.stringify(metadata));
+        
+        res.status(200).end('Ok!');
+      });   
+  } catch (error) {
+    console.log(error);
+  }
 }
-
