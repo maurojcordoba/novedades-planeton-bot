7b9a0bf040ecff5fedf37afc480021d4a8dcb15f
Mauro Cordoba <mauro.j.cordoba@gmail.com>
2023-09-12 09:48:41 -0300

    verifico que exista el archivo

--- api/chats.js
+++ api/chats.js
@@ -1,34 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-
+import { path } from 'path';
+import { readFileSync, writeFileSync,existsSync } from 'fs';
 import { tmpdir } from 'os';
 
-const path_chat = path.join(tmpdir(),'chats.json');
+const path_chat = path.join(tmpdir(), 'chats.json');
 
-function getAll(){
-  return JSON.parse(fs.readFileSync(path_chat))
+function getAll() {
+  if(!existsSync(path_chat)){
+    writeFileSync(path_chat, JSON.stringify([]));
+  }
+  return JSON.parse(readFileSync(path_chat));  
 }
 
-function push(chatId){
+function push(chatId) {
   const data = getAll();
   data.push(chatId);
   save(data);
 }
 
-function remove(chatId){
+function remove(chatId) {
   const data = getAll();
   data.splice(data.indexOf(chatId));
   save(data);
 }
 
-
-function save(data){
-  fs.writeFileSync(path_chat, JSON.stringify(data));  
+function save(data) {
+  writeFileSync(path_chat, JSON.stringify(data));
 }
 
 module.exports = {
   getAll: getAll,
   push: push,
   remove: remove,
-  save: save
-}
+  save: save,
+};
